d2eceb2bec5feb69a1c8984dcbbe7647947e7b0c
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-06-19 18:50:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const width = document.getElementById('width');
+const height = document.getElementById('height');
+
+function updateSize(){
+	height.textContent = window.innerHeight;
+	width.textContent = window.innerWidth;
+}
+
+window.addEventListener('resize', updateSize);
